3588cb6ba8b040d250713a6cd01e799ccbd5d9ad
Rahmad <rajaikhsan2021@gmail.com>
2025-02-13 17:09:05 +0700

    first commit

--- app/src/main/java/com/rahmad/newsworld/data/source/local/room/NewsDao.kt
+++ app/src/main/java/com/rahmad/newsworld/data/source/local/room/NewsDao.kt
@@ -3,13 +3,14 @@ package com.rahmad.newsworld.data.source.local.room
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.rahmad.newsworld.data.source.local.entity.NewsEntity
 
 @Dao
 interface NewsDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNews(newsEntity: NewsEntity)
 
     @Query("SELECT * FROM news where isRead = 1")
